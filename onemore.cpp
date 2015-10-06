7448847fe3ee8f7b9d756a9337358a4cb441c870
rahulnagurtha <nagurtha.rahul@gmail.com>
2015-10-06 17:45:32 +0530

    adding branch to repo

---
+++ onemore.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int t;
+    freopen("in.txt", "r", stdin);
+    cin >> t ;
+    while(t--) {
+
+    }
+    return 0;
+}
